9af2ec38bf48a5737cbc04ac8cf319ed86b11ec1
yeom <yeomfa@gmail.com>
2023-05-27 00:06:22 -0500

    App class with base methods

--- src/main.js
+++ src/main.js
@@ -1,35 +1,31 @@
 'use strict';
 
-// Restrictions
-const decitionVariables = 2; // x1, x2, ...
-const isMaximization = true;
-
-// z = '120x1 + 200x2';
-const zObj = {x1: 120, x2: 200};
-
-// x1 + x2 >= 65
-// x1 + 0 >= 23
-// 0 + x2 >= 23
-// 60x1 + 24x2 >= 23
-const restrictionsObj = [
-  {x1: 1, x2: 1, comparison: '=', result: 65},
-  {x1: 1, x2: 0, comparison: '>=', result: 23},
-  {x1: 0, x2: 1, comparison: '>=', result: 20},
-  {x1: 60, x2: 24, comparison: '<=', result: 3000},
-];
-
 // DOM
+// Buttons
 const homeBtn = document.querySelector('.home-btn');
 const solveBtn = document.querySelector('.solve-btn');
 const startBtn = document.querySelector('.start-btn');
+const optionBtns = document.querySelectorAll('.btn-option');
+const addRestBtn = document.querySelector('.add-rest-btn');
+const execSolveBtn = document.querySelector('.solve-exec-btn'); 
+
+// Sections
 const sections = document.querySelectorAll('.section');
-const btns = document.querySelectorAll('.btn');
+
+// Inputs
+const problemTypeInput = document.querySelector('.problem-type');
+const numberVariablesInput = document.querySelector('.number-variables');
+
+// Container
+const zFunctionCont = document.querySelector('.z-function-cont');
+const restrictionsCont = document.querySelector('.restrictions-container');
 
 class App {
   decitionVariables = 2;
   isMaximization = true;
   z = {};
-  restrictions = {};
+  restrictions = [];
+  OPs = [];
 
   constructor() {
     console.log('Hello from App!');
@@ -38,6 +34,15 @@ class App {
     homeBtn.addEventListener('click', this.#showSection);
     solveBtn.addEventListener('click', this.#showSection);
     startBtn.addEventListener('click', this.#showSection.bind(solveBtn));
+    addRestBtn.addEventListener('click', this.#createRestriction.bind(this));
+    execSolveBtn.addEventListener('click', this.#newOP.bind(this));
+
+    // Number variables observer 
+    const variablesObserver = new MutationObserver(this.#loadVariables.bind(this));
+    variablesObserver.observe(numberVariablesInput, {
+      characterData: true,
+      subtree: true,
+    });
   }
 
   #showSection() {
@@ -53,12 +58,122 @@ class App {
     // Remove hidden class
     section.classList.remove('hidden');
 
+    if (!btn.classList.contains('btn-option')) return;
+
     // Add active class
-    btns.forEach(btn => {
+    optionBtns.forEach(btn => {
       btn.classList.remove('active');
     })
 
     btn.classList.add('active');
+  }
+
+  #loadVariables(mutations) {
+    const [mutation] = mutations;
+    this.decitionVariables = +mutation.target.textContent;
+
+    zFunctionCont.innerHTML = '';
+    restrictionsCont.innerHTML = '';
+    
+    // Z function
+    const zTemplate = this.#createVariables();
+
+    zFunctionCont.insertAdjacentHTML('afterbegin', zTemplate);
+
+    // Restriction
+    this.#createRestriction();
+  }
+
+  #createRestriction() {
+    const comparisonTemplate = `
+      <select class="input-field comparison">
+        <option selected value=">=">≥</option>
+        <option value="<=">≤</option>
+        <option value="=">=</option>
+      </select>
+    `;
+
+    const resultTemplate = `
+      <span class="input-field result" role="textbox" contenteditable></span>
+    `;
+
+    const initDiv = `
+      <div class="variables-values restriction-cont">
+    `;
+
+    const endDiv = `
+      </div>
+    `;
+
+    const html = this.#createVariables();
+    
+    const restTemplate = initDiv + html + comparisonTemplate + resultTemplate + endDiv;
+    restrictionsCont.insertAdjacentHTML('beforeend', restTemplate);
+  }
+
+  #createVariables() {
+    const variablesArray = [];
+
+    for (let i = 0; i < this.decitionVariables; i++) {
+      const varTemplate = `
+        <div class="var">
+          <span class="input-field x-var"  data-id="${i + 1}" role="textbox" contenteditable></span>
+          <p class="variable">X<span class="subscript">${i + 1}</span></p>
+        </div>
+      `; 
+
+      variablesArray.push(varTemplate);
+    }
+
+    const html = variablesArray.join(' + ');
+
+    return html;
+  }
+
+  #newOP(e) {
+    e.preventDefault();
+
+    // Get Problem Type
+    this.isMaximization = problemTypeInput.value === 'maximization' ? true : false;
+
+    // Get Z Function
+    this.#getFunctionValues();
+
+    // Get Restrictions
+    this.#getRestrictions();
+
+    const optimizationProblem = new OptimizationProblem(this.isMaximization, this.z, this.restrictions);
+    this.OPs.push(optimizationProblem);
+  }
+
+  #getFunctionValues() {
+    const allFunctionVars = zFunctionCont.querySelectorAll('.x-var');
+    allFunctionVars.forEach(inputVar => {
+      this.z[`x${inputVar.dataset.id}`] = +inputVar.textContent;
+    })
+  }
+
+  #getRestrictions() {
+    const allRestrictions = restrictionsCont.querySelectorAll('.restriction-cont');
+    allRestrictions.forEach(rest => {
+      const restriction = {};
+
+      // Get X Vars Values
+      const allVars = rest.querySelectorAll('.x-var');
+      allVars.forEach(varInput => {
+        restriction[`x${varInput.dataset.id}`] = +varInput.textContent;
+      })
+
+      // Get Comparison
+      const comparison = rest.querySelector('.comparison').value;
+      restriction.comparison = comparison;
+
+      // Get Result
+      const result = +rest.querySelector('.result').textContent;
+      restriction.result = result;
+
+      this.restrictions.push(restriction);
+    });
   }
 }
 
